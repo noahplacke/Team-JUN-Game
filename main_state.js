bfc20d545b41dfefddd2810b2c31935ee749d022
Noah Placke <43042931+noahplacke@users.noreply.github.com>
2018-10-04 18:14:45 -0500

    floor physics/camera scroll with mouse

--- main_state.js
+++ main_state.js
@@ -10,15 +10,33 @@ mascot_raid.main_state.prototype = {
     },
 
     create: function() {
+        
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        
         //load background and floor
         var BG = game.add.sprite(0, 0, 'background');
-        var floor = game.add.sprite(0, 0, 'ground');
+        
+        //physics for immovable objects
+        immovable = game.add.group();
+        immovable.enableBody = true;
+        var floor = immovable.create(0, 0, 'ground');
+        floor.body.immovable = true;
+        
         game.world.setBounds(0, 0, 2400, 800);
 
 
     },
 
     update: function() {
+        
+        //scroll the map by dragging with mouse
+        if (this.game.input.activePointer.isDown) {	
+            if (this.game.origDragPoint) {	
+                this.game.camera.x += this.game.origDragPoint.x - this.game.input.activePointer.position.x;		
+                this.game.camera.y += this.game.origDragPoint.y - this.game.input.activePointer.position.y;	}		
+            this.game.origDragPoint = this.game.input.activePointer.position.clone();}
+            else {	
+                this.game.origDragPoint = null;}
 
     }
 }
