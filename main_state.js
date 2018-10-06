0fdcac968874e96b1fd49ee2ea96114a361806b1
Noah Placke <43042931+noahplacke@users.noreply.github.com>
2018-10-06 18:35:08 -0500

    powerup buttons

--- main_state.js
+++ main_state.js
@@ -6,6 +6,7 @@ mascot_raid.main_state.prototype = {
         game.load.image('background', 'assets/level_background.png');
         game.load.image('ground', 'assets/ground.png');
         game.load.spritesheet('tree', 'assets/trees.png');
+        game.load.spritesheet('powerups', 'assets/powerup_buttons.png', 64, 64, 4);
 
     },
 
@@ -22,7 +23,24 @@ mascot_raid.main_state.prototype = {
         var floor = immovable.create(0, 0, 'ground');
         floor.body.immovable = true;
         
+        //set edge of world
         game.world.setBounds(0, 0, 2400, 800);
+        
+        //add in power up buttons
+        buttons = game.add.group();
+        buttons.fixedToCamera = true;
+        var bevo_pow = game.add.button(50, 675, 'powerups', "", this, 0, 0, 0);
+        bevo_pow.frame = 0;
+        bevo_pow.fixedToCamera = true;
+        var matt_pow = game.add.button(120, 675, 'powerups', "", this, 1, 1, 1);
+        matt_pow.frame = 1;
+        matt_pow.fixedToCamera = true;
+        var horde_pow = game.add.button(190, 675, 'powerups', "", this, 2, 2, 2);
+        horde_pow.frame = 2;
+        horde_pow.fixedToCamera = true;
+        var rain_pow = game.add.button(260, 675, 'powerups', "", this, 3, 3, 3);
+        rain_pow.frame = 3;
+        rain_pow.fixedToCamera = true;
 
 
     },
